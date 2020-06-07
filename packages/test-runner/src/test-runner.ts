a0b2c81083c558de9eefc14c745536e3b151126b
Lars den Bakker <larsdenbakker@gmail.com>
2020-06-07 19:52:47 +0200

    feat(test-runner): add puppeteer and playwright flags

--- packages/test-runner/src/test-runner.ts
+++ packages/test-runner/src/test-runner.ts
@@ -4,6 +4,7 @@ import { startTestRunner } from '@web/test-runner-cli';
 import { createDevServer, DevServerConfig } from '@web/test-runner-dev-server';
 import { chromeLauncher } from '@web/test-runner-chrome';
 import commandLineArgs from 'command-line-args';
+import { puppeteerLauncher, playwrightLauncher } from './loadLauncher';
 
 const options: commandLineArgs.OptionDefinition[] = [
   {
@@ -14,10 +15,24 @@ const options: commandLineArgs.OptionDefinition[] = [
     name: 'preserve-symlinks',
     type: Boolean,
   },
+  {
+    name: 'puppeteer',
+    type: Boolean,
+  },
+  {
+    name: 'playwright',
+    type: Boolean,
+  },
+  {
+    name: 'browsers',
+    type: String,
+    multiple: true,
+  },
 ];
 
 const args = commandLineArgs(options, { partial: true });
 const devServerConfig: Partial<DevServerConfig> = {};
+let browsers = chromeLauncher();
 
 if ('root-dir' in args) {
   devServerConfig.rootDir = args['root-dir'];
@@ -27,9 +42,17 @@ if ('preserve-symlinks' in args) {
   devServerConfig.preserveSymlinks = !!args['preserve-symlinks'];
 }
 
+if (args.puppeteer) {
+  browsers = puppeteerLauncher();
+}
+
+if (args.playwright) {
+  browsers = playwrightLauncher(args.browsers ?? ['chromium']);
+}
+
 const defaultConfig: Partial<TestRunnerConfig> = {
   testFrameworkImport: '@web/test-runner-mocha',
-  browsers: chromeLauncher(),
+  browsers,
   server: createDevServer(devServerConfig),
 };
 
