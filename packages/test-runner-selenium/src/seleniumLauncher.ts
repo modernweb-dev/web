c97efc5b91dcfd4139921e7c692bcadbc2969daf
Lars den Bakker <larsdenbakker@gmail.com>
2020-06-18 08:31:07 +0200

    fix(test-runner-selenium): don't run tests in parallel

--- packages/test-runner-selenium/src/seleniumLauncher.ts
+++ packages/test-runner-selenium/src/seleniumLauncher.ts
@@ -5,6 +5,73 @@ export interface SeleniumLauncherArgs {
   driverBuilder: Builder;
 }
 
+export class SeleniumLauncher {
+  private activePages = new Map<string, string>();
+  private inactivePages: string[] = [];
+  private sessionsQueue: { session: TestSession; url: string }[] = [];
+  private driver: undefined | WebDriver;
+  private debugDriver: undefined | WebDriver = undefined;
+  private currentSession: string | undefined;
+
+  constructor(private driverBuilder: Builder) {}
+
+  async start() {
+    this.driver = await this.driverBuilder.build();
+
+    const cap = await this.driver.getCapabilities();
+    return [`${cap.getPlatform()} ${cap.getBrowserName()} ${cap.getBrowserVersion()}`];
+  }
+
+  async stop() {
+    try {
+      await this.driver?.quit();
+      await this.debugDriver?.quit();
+
+      this.driver = undefined;
+      this.debugDriver = undefined;
+    } catch {
+      //
+    }
+  }
+
+  async startSession(session: TestSession, url: string) {
+    this.sessionsQueue.push({ session, url });
+
+    if (!this.currentSession) {
+      return this._runNextQueuedSession();
+    }
+  }
+
+  async _runNextQueuedSession() {
+    const next = this.sessionsQueue.shift();
+    if (!next) {
+      return;
+    }
+
+    if (!this.driver) {
+      throw new Error('Browser is closed');
+    }
+
+    this.currentSession = next.session.id;
+    await this.driver.get(next.url);
+  }
+
+  stopSession(session: TestSession) {
+    if (this.currentSession === session.id) {
+      this.currentSession = undefined;
+      this._runNextQueuedSession();
+    }
+  }
+
+  async startDebugSession(session: TestSession, url: string) {
+    if (this.debugDriver) {
+      await this.debugDriver.quit();
+    }
+    this.debugDriver = await this.driverBuilder.build();
+    await this.debugDriver.navigate().to(url);
+  }
+}
+
 export function seleniumLauncher(args: SeleniumLauncherArgs) {
   if (!args?.driverBuilder) {
     throw new Error(`Selenium launcher requires a driverBuilder property.`);
@@ -14,92 +81,5 @@ export function seleniumLauncher(args: SeleniumLauncherArgs) {
     throw new Error(`driverBuild must be a Builder`);
   }
 
-  const activePages = new Map<string, string>();
-  const inactivePages: string[] = [];
-  const sessionsQueue: { session: TestSession; url: string }[] = [];
-  let runningQueuedSessions = false;
-  let driver: undefined | WebDriver;
-  let debugDriver: undefined | WebDriver = undefined;
-
-  return {
-    async start() {
-      driver = await args.driverBuilder.build();
-      const cap = await driver.getCapabilities();
-      return [`${cap.getPlatform()} ${cap.getBrowserName()} ${cap.getBrowserVersion()}`];
-    },
-
-    async stop() {
-      try {
-        await driver?.quit();
-        await debugDriver?.quit();
-
-        driver = undefined;
-        debugDriver = undefined;
-      } catch {
-        //
-      }
-    },
-
-    async startSession(session: TestSession, url: string) {
-      sessionsQueue.push({ session, url });
-      if (runningQueuedSessions) {
-        return;
-      }
-
-      return this._runQueuedSessions();
-    },
-
-    async _runQueuedSessions() {
-      try {
-        runningQueuedSessions = true;
-        const next = sessionsQueue.shift();
-        if (!next) {
-          throw new Error('Did not find a next session to run');
-        }
-
-        if (!driver) {
-          throw new Error('Browser is closed');
-        }
-
-        let page: string;
-        if (inactivePages.length === 0) {
-          const handlesBefore = await driver.getAllWindowHandles();
-          await driver.executeScript(`window.open('')`);
-          const handlesAfter = await driver.getAllWindowHandles();
-          page = handlesAfter.find(h => !handlesBefore.includes(h))!;
-          if (!page) {
-            throw new Error('Selenium did not open a new browser window.');
-          }
-        } else {
-          page = inactivePages.pop()!;
-        }
-
-        activePages.set(next.session.id, page);
-        await driver.switchTo().window(page);
-        await driver.navigate().to(next.url);
-      } finally {
-        if (sessionsQueue.length > 0) {
-          await this._runQueuedSessions();
-        } else {
-          runningQueuedSessions = false;
-        }
-      }
-    },
-
-    stopSession(session: TestSession) {
-      const page = activePages.get(session.id);
-      if (page) {
-        activePages.delete(session.id);
-        inactivePages.push(page);
-      }
-    },
-
-    async startDebugSession(session: TestSession, url: string) {
-      if (debugDriver) {
-        await debugDriver.quit();
-      }
-      debugDriver = await args.driverBuilder.build();
-      await debugDriver.navigate().to(url);
-    },
-  };
+  return new SeleniumLauncher(args.driverBuilder);
 }
