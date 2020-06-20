cb7475debfa67f7c892a9888eb6043dec55a6387
Lars den Bakker <larsdenbakker@gmail.com>
2020-06-20 16:54:56 +0200

    chore: align versions in repository

---
+++ .github/workflows/ci-postinstall.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const testRunnerBin = path.resolve('packages', 'test-runner', 'dist', 'test-runner.js');
+const projectsDir = path.resolve('demo', 'projects');
+const projects = fs.readdirSync(projectsDir);
+
+for (const project of projects) {
+  const linkDestination = path.resolve(projectsDir, project, 'node_modules', '.bin', 'wtr');
+  fs.linkSync(testRunnerBin, linkDestination);
+}
