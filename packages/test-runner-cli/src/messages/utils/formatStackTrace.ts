93dbfe544509b35d40af82a6b2dce2ba6accc9a5
Lars den Bakker <larsdenbakker@gmail.com>
2020-07-08 21:15:26 +0200

    fix(test-runner-cli): remove minified test framework from stack trace

--- packages/test-runner-cli/src/messages/utils/formatStackTrace.ts
+++ packages/test-runner-cli/src/messages/utils/formatStackTrace.ts
@@ -1,5 +1,10 @@
 import { TestResultError } from '@web/test-runner-core';
 import { replaceRelativeStackFilePath } from './replaceRelativeStackFilePath';
+import { constants } from '@web/test-runner-core';
+const { PARAM_SESSION_ID } = constants;
+
+const REGEXP_TEST_FRAMEWORK_STACK = /.+__web-test-runner__\/.+/;
+const REGEXP_WTR_SESSION_ID = new RegExp(`\\?${PARAM_SESSION_ID}=(\\d|[a-z]|-)*`);
 
 export function formatStackTrace(error: TestResultError, rootDir: string, serverAddress: string) {
   if (!error.stack) {
@@ -15,9 +20,11 @@ export function formatStackTrace(error: TestResultError, rootDir: string, server
   }
 
   return strings
+    .filter(str => !REGEXP_TEST_FRAMEWORK_STACK.test(str))
     .map((str, i) => {
+      const withoutSessionId = str.replace(REGEXP_WTR_SESSION_ID, '');
       // ensure there is an indentation of 2 spaces
-      const trimmedString = `${' '.repeat(i === 0 ? 0 : 2)}${str.trim()}`;
+      const trimmedString = `${' '.repeat(i === 0 ? 0 : 2)}${withoutSessionId.trim()}`;
 
       // replace browser url with relative file path
       return replaceRelativeStackFilePath(trimmedString, rootDir, serverAddress);
