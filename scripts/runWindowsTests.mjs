62ff8b24ca47f80256214a4c6b555896ea9b0bcd
Lars den Bakker <larsdenbakker@gmail.com>
2020-08-09 12:41:07 +0200

    chore: add changeset

--- scripts/runWindowsTests.mjs
+++ scripts/runWindowsTests.mjs
@@ -3,9 +3,4 @@ import { runWorkspacesScripts } from './runWorkspacesScripts.mjs';
 runWorkspacesScripts({
   script: 'test:ci',
   concurrency: 1,
-  filteredPackages: [
-    // playwright can't install browsers on my windows VM
-    // 'test-runner-playwright',
-    // 'test-runner-selenium',
-  ],
 });
