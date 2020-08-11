e827c9286d05a0b3114818f677a991ff21ad3567
Lars den Bakker <larsdenbakker@gmail.com>
2020-08-11 23:06:24 +0200

    feat(test-runner-cli): add --help command

--- packages/test-runner-cli/src/config/readCliArgsConfig.ts
+++ packages/test-runner-cli/src/config/readCliArgsConfig.ts
@@ -1,4 +1,5 @@
-import commandLineArgs, { OptionDefinition } from 'command-line-args';
+import commandLineArgs from 'command-line-args';
+import commandLineUsage, { OptionDefinition } from 'command-line-usage';
 import camelCase from 'camelcase';
 
 const defaultOptions: OptionDefinition[] = [
@@ -7,30 +8,42 @@ const defaultOptions: OptionDefinition[] = [
     type: String,
     multiple: true,
     defaultOption: true,
-  },
-  {
-    name: 'watch',
-    type: Boolean,
-  },
-  {
-    name: 'coverage',
-    type: Boolean,
-  },
-  {
-    name: 'concurrency',
-    type: Boolean,
-  },
-  {
-    name: 'config',
-    type: String,
-  },
-  {
-    name: 'static-logging',
-    type: Boolean,
+    description: 'Test files to run',
   },
   {
     name: 'root-dir',
     type: String,
+    description: 'Root directory to serve files from.',
+  },
+  {
+    name: 'watch',
+    type: Boolean,
+    description: 'Reload tests on file changes',
+  },
+  {
+    name: 'coverage',
+    type: Boolean,
+    description: 'Check for code coverage. Slows down testing.',
+  },
+  {
+    name: 'concurrency',
+    type: Number,
+    description: 'Amount of tests to run concurrently. Defaults to 10.',
+  },
+  {
+    name: 'config',
+    type: String,
+    description: 'Location to read config file from.',
+  },
+  {
+    name: 'static-logging',
+    type: Boolean,
+    description: 'Disables rendering a progress bar dynamically to the terminal.',
+  },
+  {
+    name: 'help',
+    type: Boolean,
+    description: 'Print help commands',
   },
 ];
 
@@ -38,7 +51,27 @@ export function readCliArgsConfig<T>(
   extraOptions: OptionDefinition[] = [],
   argv = process.argv,
 ): Partial<T> {
-  const cliArgs = commandLineArgs([...defaultOptions, ...extraOptions], { argv });
+  const options = [...defaultOptions, ...extraOptions];
+  const cliArgs = commandLineArgs(options, { argv });
+
+  if ('help' in cliArgs) {
+    /* eslint-disable-next-line no-console */
+    console.log(
+      commandLineUsage([
+        {
+          header: 'Web Test Runner',
+          content: 'Test runner for web applications.',
+        },
+        {
+          header: 'Usage',
+          content: 'web-test-runner [options...]' + '\nwtr [options...]',
+        },
+        { header: 'Options', optionList: options },
+      ]),
+    );
+    process.exit();
+  }
+
   const cliArgsConfig: Partial<T> = {};
 
   for (const [key, value] of Object.entries(cliArgs)) {
