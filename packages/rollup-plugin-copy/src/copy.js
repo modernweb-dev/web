df89af55b7b9995a22fbb9396b234c9509f47127
Thomas Allmer <d4kmor@gmail.com>
2020-08-30 20:33:00 +0200

    chore: schedule a release for rollup-plugin-copy

--- packages/rollup-plugin-copy/src/copy.js
+++ packages/rollup-plugin-copy/src/copy.js
@@ -3,12 +3,22 @@ const path = require('path');
 const { patternsToFiles } = require('./patternsToFiles.js');
 
 /**
- * Copies all files from the given patterns retaining relative pathes to the root folder
+ * Copies all files from the given patterns retaining relative paths relative to the root directory.
+ *
+ * @example
+ * // Copy all svg, jpg and json files
+ * copy({ patterns: '**\/*.{svg,jpg,json}' })
+ * // Copy svg files from two different folders
+ * copy({ patterns: ['icons\/*.svg', 'test/icons\/*.svg'] })
+ * // Copy all svg files relative to given rootDir
+ * copy({ patterns: '**\/*.svg', rootDir: './my-path/' })
+ * // you can combine it with path.resolve
+ * copy({ patterns: '**\/*.svg', rootDir: path.resolve('./my-path/')  })
  *
  * @param {object} options
- * @param {string|string[]} options.patterns
- * @param {string} [options.rootDir]
- * @return {import('rollup').Plugin}
+ * @param {string|string[]} options.patterns Single glob or an array of globs
+ * @param {string} [options.rootDir] Defaults to current working directory
+ * @return {import('rollup').Plugin} A Rollup Plugin
  */
 function copy({ patterns = [], rootDir = process.cwd() }) {
   const resolvedRootDir = path.resolve(rootDir);
