2802df64db85c93c2062152ff4c214197e962fd5
Lars den Bakker <larsdenbakker@gmail.com>
2020-09-07 23:27:36 +0200

    fix(test-runner): handle infinite loops when reloading the page

--- packages/test-runner/demo/selenium.config.js
+++ packages/test-runner/demo/selenium.config.js
@@ -22,17 +22,17 @@ module.exports = {
   nodeResolve: true,
 
   browsers: [
-    seleniumLauncher({
-      driverBuilder: new Builder()
-        .forBrowser('chrome')
-        .setChromeOptions(new ChromeOptions().headless())
-        .usingServer('http://localhost:4444/wd/hub'),
-    }),
+    // seleniumLauncher({
+    //   driverBuilder: new Builder()
+    //     .forBrowser('chrome')
+    //     .setChromeOptions(new ChromeOptions().headless())
+    //     .usingServer('http://localhost:4444/wd/hub'),
+    // }),
 
     seleniumLauncher({
       driverBuilder: new Builder()
         .forBrowser('firefox')
-        .setFirefoxOptions(new FirefoxOptions().headless())
+        // .setFirefoxOptions(new FirefoxOptions().headless())
         .usingServer('http://localhost:4444/wd/hub'),
     }),
   ],
