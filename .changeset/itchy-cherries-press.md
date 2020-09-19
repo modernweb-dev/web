994f6e4d461f1137e0e6f9cac7e4c3b82700a5dd
Lars den Bakker <larsdenbakker@gmail.com>
2020-09-19 12:33:09 +0200

    feat(test-runner-selenium): improve generating browser name

---
+++ .changeset/itchy-cherries-press.md
@@ -0,0 +1,5 @@
+---
+'@web/test-runner-selenium': patch
+---
+
+improved the way browser names are generated
--- packages/test-runner-selenium/src/utils.ts
+++ packages/test-runner-selenium/src/utils.ts
@@ -1,3 +1,5 @@
+import { Capabilities } from 'selenium-webdriver';
+
 /**
  * Wraps a Promise with a timeout, rejecing the promise with the timeout.
  */
@@ -18,4 +20,26 @@ export function withTimeout<T>(promise: Promise<T>, message: string, timeout: nu
         clearTimeout(timeoutId);
       });
   });
+}
+
+const isDefined = (_: unknown) => !!_;
+
+function getPlatform(c: Capabilities) {
+  return (
+    c.getPlatform() ||
+    c.get('platform') ||
+    [c.get('os'), c.get('os_version')].filter(isDefined).join(' ')
+  );
+}
+
+function getBrowser(c: Capabilities) {
+  return c.getBrowserName() || c.get('browserName') || c.get('browser_name');
+}
+
+function getBrowserVersion(c: Capabilities) {
+  return c.getBrowserVersion() || c.get('browserVersion') || c.get('browser_version');
+}
+
+export function getBrowserName(c: Capabilities) {
+  return [getPlatform(c), getBrowser(c), getBrowserVersion(c)].filter(_ => _).join(' ');
 }
