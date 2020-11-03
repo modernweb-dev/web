8a48245a615b0c9203d497a0b4eb76f788588ac2
Lars den Bakker <larsdenbakker@gmail.com>
2020-11-03 21:48:21 +0100

    feat(rollup-plugin-polyfills-loader): initial implementation

---
+++ packages/rollup-plugin-polyfills-loader/src/types.d.ts
@@ -0,0 +1,16 @@
+import { PolyfillsConfig, FileType } from '@web/polyfills-loader';
+
+export interface OutputConfig {
+  name: string;
+  type?: FileType;
+}
+
+export interface LegacyOutputConfig extends OutputConfig {
+  test: string;
+}
+
+export interface RollupPluginPolyfillsLoaderConfig {
+  modernOutput?: OutputConfig;
+  legacyOutput?: LegacyOutputConfig | LegacyOutputConfig[];
+  polyfills?: PolyfillsConfig;
+}
