cba758efc54f01a7992bd10674830d6aff5fe9fc
Lars den Bakker <larsdenbakker@gmail.com>
2020-11-05 17:50:46 +0100

    feat(dev-server-storybook): throw when missing plugin configuration

---
+++ .changeset/little-toes-report.md
@@ -0,0 +1,5 @@
+---
+'@web/dev-server-storybook': patch
+---
+
+feat(dev-server-storybook): throw when no config is given
--- packages/dev-server-storybook/src/shared/config/validatePluginConfig.ts
+++ packages/dev-server-storybook/src/shared/config/validatePluginConfig.ts
@@ -4,6 +4,10 @@ import { createError } from '../utils';
 const types = ['preact', 'web-components'];
 
 export function validatePluginConfig(pluginConfig: StorybookPluginConfig) {
+  if (!pluginConfig) {
+    throw createError('Missing plugin configuration.');
+  }
+
   if (typeof pluginConfig.type !== 'string') {
     throw createError('Missing project type in config.');
   }
