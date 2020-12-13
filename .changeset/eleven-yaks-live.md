1f6ad0af40d4a359d07b89fea20d1360aef21525
Lars den Bakker <larsdenbakker@gmail.com>
2020-12-13 16:45:36 +0100

    feat(test-runner): reuse common plugins from dev server

---
+++ .changeset/eleven-yaks-live.md
@@ -0,0 +1,6 @@
+---
+'@web/dev-server': patch
+'@web/test-runner': patch
+---
+
+reuse dev server plugins in test runner
--- packages/dev-server/src/index.ts
+++ packages/dev-server/src/index.ts
@@ -2,3 +2,5 @@ export { startDevServer } from './startDevServer';
 export { DevServerConfig } from './config/DevServerConfig';
 export { mergeConfigs } from './config/mergeConfigs';
 export { DevServerStartError } from './DevServerStartError';
+export { nodeResolvePlugin } from './plugins/nodeResolvePlugin';
+export { esbuildPlugin } from './plugins/esbuildPlugin';
