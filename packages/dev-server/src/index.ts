b327702f978f9970112e74d005170cd730e56d7c
Lars den Bakker <larsdenbakker@gmail.com>
2020-12-13 17:01:00 +0100

    feat(dev-server): export plugins

--- packages/dev-server/src/index.ts
+++ packages/dev-server/src/index.ts
@@ -2,3 +2,5 @@ export { startDevServer } from './startDevServer';
 export { DevServerConfig } from './config/DevServerConfig';
 export { mergeConfigs } from './config/mergeConfigs';
 export { DevServerStartError } from './DevServerStartError';
+export { esbuildPlugin } from './plugins/esbuildPlugin';
+export { nodeResolvePlugin } from './plugins/nodeResolvePlugin';
