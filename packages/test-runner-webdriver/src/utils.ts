5289a528311fee99afbbd68889555bb823535ce8
Lars den Bakker <larsdenbakker@gmail.com>
2020-12-23 12:00:45 +0100

    chore: update webdriver io package

--- packages/test-runner-webdriver/src/utils.ts
+++ packages/test-runner-webdriver/src/utils.ts
@@ -1,17 +1,17 @@
-import { DesiredCapabilities } from 'webdriver';
+import WebDriver from 'webdriver';
 
-function getPlatform(c: DesiredCapabilities): string | undefined {
+function getPlatform(c: WebDriver.DesiredCapabilities): string | undefined {
   return c.platformName || c.platform;
 }
 
-export function getBrowserName(c: DesiredCapabilities): string | undefined {
+export function getBrowserName(c: WebDriver.DesiredCapabilities): string | undefined {
   return c.browserName;
 }
 
-function getBrowserVersion(c: DesiredCapabilities): string | undefined {
+function getBrowserVersion(c: WebDriver.DesiredCapabilities): string | undefined {
   return c.browserVersion || c.version;
 }
 
-export function getBrowserLabel(c: DesiredCapabilities): string {
+export function getBrowserLabel(c: WebDriver.DesiredCapabilities): string {
   return [getPlatform(c), getBrowserName(c), getBrowserVersion(c)].filter(_ => _).join(' ');
 }
