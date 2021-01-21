73fb30737c09d42cbd3ff83ffc1f69f2c536c030
Thomas Allmer <d4kmor@gmail.com>
2021-01-21 18:37:29 +0100

    chore: add search ui, social images, link check

--- rocket.config.mjs
+++ rocket.config.mjs
@@ -1,11 +1,17 @@
-import { rocketLaunch } from '@d4kmor/launch';
-import { rocketSearch } from '@d4kmor/search';
-import { absoluteBaseUrlNetlify } from '@d4kmor/core/helpers';
+import { rocketLaunch } from '@rocket/launch';
+import { rocketBlog } from '@rocket/blog';
+import { rocketSearch } from '@rocket/search';
+import { absoluteBaseUrlNetlify } from '@rocket/core/helpers';
+import { adjustPluginOptions } from 'plugins-manager';
+import path from 'path';
+
+const serviceWorkerPath = path.resolve('./_site-dev/sw.js');
 
 export default {
-  themes: [rocketLaunch(), rocketSearch()],
-  build: {
-    absoluteBaseUrl: absoluteBaseUrlNetlify('http://localhost:8080'),
-    serviceWorkerFileName: 'sw.js',
-  },
+  presets: [rocketLaunch(), rocketBlog(), rocketSearch()],
+  absoluteBaseUrl: absoluteBaseUrlNetlify('http://localhost:8080'),
+  setupBuildPlugins: [
+    adjustPluginOptions('workbox', { swDest: serviceWorkerPath }),
+    adjustPluginOptions('html', { serviceWorkerPath }),
+  ],
 };
