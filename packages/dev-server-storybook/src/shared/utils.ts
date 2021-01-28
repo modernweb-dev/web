f66aaa8021cfa277d7a528381efb64bb9255ec6a
Lars den Bakker <larsdenbakker@gmail.com>
2021-01-28 20:59:40 +0100

    fix(dev-server-storybook): fix checking path on windows

--- packages/dev-server-storybook/src/shared/utils.ts
+++ packages/dev-server-storybook/src/shared/utils.ts
@@ -1,11 +1,12 @@
 import path from 'path';
+import pathIsInside from 'path-is-inside';
 
 export function createError(msg: string) {
   return new Error(`[@web/dev-server-storybook] ${msg}`);
 }
 
 export function createBrowserImport(rootDir: string, filePath: string) {
-  if (!filePath.startsWith(rootDir)) {
+  if (!pathIsInside(filePath, rootDir)) {
     throw createError(
       `The file ${filePath} is not accessible by the browser because it is outside the root directory ${rootDir}. ` +
         'Change the rootDir option to include this directory.',
