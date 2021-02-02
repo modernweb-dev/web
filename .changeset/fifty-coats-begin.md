d75676de8ccb2f03472f6b9e5852106c00363479
Lars den Bakker <larsdenbakker@gmail.com>
2021-02-02 18:19:41 +0100

    fix(dev-server-storybook): normalize paths received from storybook

---
+++ .changeset/fifty-coats-begin.md
@@ -0,0 +1,5 @@
+---
+'@web/dev-server-storybook': patch
+---
+
+normalize paths received from globby
--- packages/dev-server-storybook/src/shared/stories/findStories.ts
+++ packages/dev-server-storybook/src/shared/stories/findStories.ts
@@ -5,7 +5,8 @@ import { createBrowserImport, createError } from '../utils';
 
 export async function findStories(rootDir: string, mainJsPath: string, storiesPatterns: string[]) {
   const mainJsDir = path.dirname(mainJsPath);
-  const storyFilePaths = await globby(storiesPatterns, { cwd: mainJsDir, absolute: true });
+  const storyPaths = await globby(storiesPatterns, { cwd: mainJsDir, absolute: false });
+  const storyFilePaths = storyPaths.map(p => path.join(mainJsDir, p.split('/').join(path.sep)));
 
   if (storyFilePaths.length === 0) {
     throw createError(`Could not find any stories with pattern ${storiesPatterns}`);
