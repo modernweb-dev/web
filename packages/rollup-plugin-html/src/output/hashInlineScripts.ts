d9f3ac4bd9a5e87d1eb6832aa4b42779a4e5cffe
Joren Broekema <joren.broekema@ing.com>
2021-03-03 16:31:21 +0100

    chore: add functionality for reusing existing meta CSP tag

--- packages/rollup-plugin-html/src/output/hashInlineScripts.ts
+++ packages/rollup-plugin-html/src/output/hashInlineScripts.ts
@@ -1,14 +1,27 @@
-import { Node, Document } from 'parse5';
+import { Node, Document, DefaultTreeElement } from 'parse5';
 import {
+  findElement,
   findElements,
   getTagName,
   hasAttribute,
+  getAttribute,
   getTextContent,
   createElement,
   findNode,
   prepend,
+  setAttribute,
 } from '@web/parse5-utils';
 import crypto from 'crypto';
+
+function isMetaCSPTag(node: Node) {
+  if (
+    getTagName(node) === 'meta' &&
+    getAttribute(node, 'http-equiv') === 'Content-Security-Policy'
+  ) {
+    return true;
+  }
+  return false;
+}
 
 function isInlineScript(node: Node) {
   if (getTagName(node) === 'script' && !hasAttribute(node, 'src')) {
@@ -17,10 +30,101 @@ function isInlineScript(node: Node) {
   return false;
 }
 
+/**
+ * Parses Meta CSP Content string as an object so we can easily mutate it in JS
+ * E.g.:
+ *
+ * "default-src 'self'; prefetch-src 'self'; upgrade-insecure-requests; style-src 'self' 'unsafe-inline';"
+ *
+ * becomes
+ *
+ * {
+ *   'default-src': ["'self'"],
+ *   'prefetch-src': ["'self'"],
+ *   'upgrade-insecure-requests': [],
+ *   'style-src': ["'self'", "'unsafe-inline'"]
+ * }
+ *
+ */
+function parseMetaCSPContent(content: string): { [key: string]: string[] } {
+  return content.split(';').reduce((acc, curr) => {
+    const trimmed = curr.trim();
+    if (!trimmed) {
+      return acc;
+    }
+    const splitItem = trimmed.split(' ');
+    const [, ...values] = splitItem;
+    return {
+      ...acc,
+      [splitItem[0]]: values,
+    };
+  }, {});
+}
+
+/**
+ * Serializes
+ *
+ * {
+ *   'default-src': ["'self'"],
+ *   'prefetch-src': ["'self'"],
+ *   'upgrade-insecure-requests': [],
+ *   'style-src': ["'self'", "'unsafe-inline'"]
+ * }
+ *
+ * back to
+ *
+ * "default-src 'self'; prefetch-src 'self'; upgrade-insecure-requests; style-src 'self' 'unsafe-inline';"
+ */
+function serializeMetaCSPContent(data: { [key: string]: string[] }): string {
+  const dataEntries = Object.entries(data);
+  return dataEntries.reduce((accOuter, currOuter, indexOuter) => {
+    let suffixOuter = ' ';
+    let sep = ' ';
+
+    // If there are no items for this key
+    if (currOuter[1].length === 0) {
+      suffixOuter = '; ';
+      sep = '';
+    }
+
+    // Don't insert space suffix when it is the last item
+    if (indexOuter === dataEntries.length - 1) {
+      suffixOuter = '';
+    }
+
+    return `${accOuter}${currOuter[0]}${sep}${currOuter[1].reduce(
+      (accInner, currInner, indexInner) => {
+        let suffixInner = ' ';
+        if (indexInner === currOuter[1].length - 1) {
+          suffixInner = ';';
+        }
+        return `${accInner}${currInner}${suffixInner}`;
+      },
+      '',
+    )}${suffixOuter}`;
+  }, '');
+}
+
+function injectCSPScriptRules(metaCSPEl: DefaultTreeElement, hashes: string[]) {
+  const content = getAttribute(metaCSPEl, 'content');
+  if (content) {
+    const data = parseMetaCSPContent(content);
+
+    if (Array.isArray(data['script-src'])) {
+      data['script-src'].push(...hashes);
+    } else {
+      data['script-src'] = ["'self'", ...hashes];
+    }
+
+    const newContent = serializeMetaCSPContent(data);
+    setAttribute(metaCSPEl, 'content', newContent);
+  }
+}
+
 function injectCSPMetaTag(document: Document, hashes: string[]) {
   const metaTag = createElement('meta', {
     'http-equiv': 'Content-Security-Policy',
-    content: `script-src 'self' ${hashes.join(' ')}`,
+    content: `script-src 'self' ${hashes.join(' ')};`,
   });
   const head = findNode(document, node => node.nodeName === 'head');
   if (head) {
@@ -29,9 +133,10 @@ function injectCSPMetaTag(document: Document, hashes: string[]) {
 }
 
 export function hashInlineScripts(document: Document) {
-  const nodes = findElements(document, isInlineScript);
+  const metaCSPEl = findElement(document, isMetaCSPTag);
+  const inlineScripts = findElements(document, isInlineScript);
   const hashes: string[] = [];
-  nodes.forEach(node => {
+  inlineScripts.forEach(node => {
     if (node.childNodes[0]) {
       const scriptContent = getTextContent(node.childNodes[0]);
       const hash = crypto.createHash('sha256').update(scriptContent).digest('base64');
@@ -39,6 +144,10 @@ export function hashInlineScripts(document: Document) {
     }
   });
   if (hashes.length > 0) {
-    injectCSPMetaTag(document, hashes);
+    if (metaCSPEl) {
+      injectCSPScriptRules(metaCSPEl, hashes);
+    } else {
+      injectCSPMetaTag(document, hashes);
+    }
   }
 }
