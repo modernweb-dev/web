a2e89142e418fcb126e755c8b38b0653cc2179a0
Scott <52388100+ScottAwesome@users.noreply.github.com>
2021-04-27 08:51:07 -0700

    Updated browser-targets.ts wording
    The purpose of this PR is to update the wording when certain browsers are not found, so the end user knows which specific browser was not found.

--- packages/dev-server-esbuild/src/browser-targets.ts
+++ packages/dev-server-esbuild/src/browser-targets.ts
@@ -13,13 +13,13 @@ function createModernTarget() {
     if (!latestChrome) throw new Error('Could not find latest Chrome major version');
 
     const latestEdge = getLatestStableMajor(browsers.edge.releases);
-    if (!latestEdge) throw new Error('Could not find latest Chrome major version');
+    if (!latestEdge) throw new Error('Could not find latest Edge major version');
 
     const latestSafari = getLatestStableMajor(browsers.safari.releases);
     if (!latestSafari) throw new Error('Could not find latest Safari major version');
 
     const latestFirefox = getLatestStableMajor(browsers.firefox.releases);
-    if (!latestFirefox) throw new Error('Could not find latest Chrome major version');
+    if (!latestFirefox) throw new Error('Could not find latest Firefox major version');
 
     return [
       `chrome${latestChrome - 1}`,
