c85151a2fc66b62df0088d67d8865ffaeea6ade8
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-05-21 12:26:31 +0200

    Add down and up commands

--- packages/test-runner-commands/src/sendKeysPlugin.ts
+++ packages/test-runner-commands/src/sendKeysPlugin.ts
@@ -2,29 +2,57 @@ import { TestRunnerPlugin } from '@web/test-runner-core';
 import type { ChromeLauncher, puppeteerCore } from '@web/test-runner-chrome';
 import type { PlaywrightLauncher } from '@web/test-runner-playwright';
 
+type TypePayload = { type: string; };
+type PressPayload = { press: string; };
+type DownPayload = { down: string; };
+type UpPayload = { up: string; };
+
 export type SendKeysPayload =
-  | { type: string; press?: undefined }
-  | { type?: undefined; press: string };
+  | TypePayload
+  | PressPayload
+  | DownPayload
+  | UpPayload;
 
 function isObject(payload: unknown): payload is Record<string, unknown> {
   return payload != null && typeof payload === 'object';
 }
 
 function isSendKeysPayload(payload: unknown): boolean {
+  const validOptions = ['type', 'press', 'down', 'up'];
+
   if (!isObject(payload)) throw new Error('You must provide a `SendKeysPayload` object');
-  if (!!payload.type && !!payload.press)
+
+  const numberOfValidOptions = Object.keys(payload).filter(key => validOptions.includes(key)).length;
+  const unknownOptions = Object.keys(payload).filter(key => !validOptions.includes(key));
+
+  if (numberOfValidOptions > 1)
     throw new Error(
-      'You must provide ONLY one of the `type` or `press` properties for pass to the browser runner.',
+      `You must provide ONLY one of the following properties to pass to the browser runner: ${validOptions.join(', ')}.`,
     );
-  if (!payload.type && !payload.press)
+  if (numberOfValidOptions === 0)
     throw new Error(
-      'You mist provide one of the `type` or `press` properties for pass to the browser runner.',
+      `You must provide one of the following properties to pass to the browser runner: ${validOptions.join(', ')}.`,
     );
-  const payloadKeys = Object.keys(payload).filter(key => key !== 'press' && key !== 'type');
-  if (payloadKeys.length) {
-    throw new Error('Unknown options `' + payloadKeys.join(', ') + '` present.');
+  if (unknownOptions.length > 0) {
+    throw new Error('Unknown options `' + unknownOptions.join(', ') + '` present.');
   }
   return true;
+}
+
+function isTypePayload(payload: SendKeysPayload): payload is TypePayload {
+  return 'type' in payload;
+}
+
+function isPressPayload(payload: SendKeysPayload): payload is PressPayload {
+  return 'press' in payload;
+}
+
+function isDownPayload(payload: SendKeysPayload): payload is DownPayload {
+  return 'down' in payload;
+}
+
+function isUpPayload(payload: SendKeysPayload): payload is UpPayload {
+  return 'up' in payload;
 }
 
 export function sendKeysPlugin(): TestRunnerPlugin<SendKeysPayload> {
@@ -38,11 +66,17 @@ export function sendKeysPlugin(): TestRunnerPlugin<SendKeysPayload> {
         // handle specific behavior for playwright
         if (session.browser.type === 'playwright') {
           const page = (session.browser as PlaywrightLauncher).getPage(session.id);
-          if (payload.type) {
+          if (isTypePayload(payload)) {
             await page.keyboard.type(payload.type);
             return true;
-          } else if (payload.press) {
+          } else if (isPressPayload(payload)) {
             await page.keyboard.press(payload.press);
+            return true;
+          } else if (isDownPayload(payload)) {
+            await page.keyboard.down(payload.down);
+            return true;
+          } else if (isUpPayload(payload)) {
+            await page.keyboard.up(payload.up);
             return true;
           }
         }
@@ -50,11 +84,17 @@ export function sendKeysPlugin(): TestRunnerPlugin<SendKeysPayload> {
         // handle specific behavior for puppeteer
         if (session.browser.type === 'puppeteer') {
           const page = (session.browser as ChromeLauncher).getPage(session.id);
-          if (payload.type) {
+          if (isTypePayload(payload)) {
             await page.keyboard.type(payload.type);
             return true;
-          } else if (payload.press) {
+          } else if (isPressPayload(payload)) {
             await page.keyboard.press(payload.press as puppeteerCore.KeyInput);
+            return true;
+          } else if (isDownPayload(payload)) {
+            await page.keyboard.down(payload.down as puppeteerCore.KeyInput);
+            return true;
+          } else if (isUpPayload(payload)) {
+            await page.keyboard.up(payload.up as puppeteerCore.KeyInput);
             return true;
           }
         }
