41e80116a5d04efb18a69fdb4cbce6e95c979757
Thomas Allmer <d4kmor@gmail.com>
2021-06-09 08:34:22 +0200

    chore: reenable website deploys by updating rocket to latest

--- rocket.config.mjs
+++ rocket.config.mjs
@@ -2,16 +2,9 @@ import { rocketLaunch } from '@rocket/launch';
 import { rocketBlog } from '@rocket/blog';
 import { rocketSearch } from '@rocket/search';
 import { absoluteBaseUrlNetlify } from '@rocket/core/helpers';
-import { adjustPluginOptions } from 'plugins-manager';
-import path from 'path';
-
-const serviceWorkerPath = path.resolve('./_site/sw.js');
 
 export default {
   presets: [rocketLaunch(), rocketBlog(), rocketSearch()],
   absoluteBaseUrl: absoluteBaseUrlNetlify('http://localhost:8080'),
-  setupBuildPlugins: [
-    adjustPluginOptions('workbox', { swDest: serviceWorkerPath }),
-    adjustPluginOptions('html', { serviceWorkerPath }),
-  ],
+  serviceWorkerName: 'sw.js',
 };
