895306047cdfc4e8de315402100eb88d9014c82a
web-padawan <iamkulykov@gmail.com>
2021-11-05 19:04:10 +0200

    test: skip flaky Playwright tests on Windows

--- packages/test-runner-playwright/test/playwrightLauncher.test.ts
+++ packages/test-runner-playwright/test/playwrightLauncher.test.ts
@@ -1,3 +1,4 @@
+import os from 'os';
 import { runIntegrationTests } from '../../../integration/test-runner';
 import { playwrightLauncher } from '../src/index';
 
@@ -37,45 +38,48 @@ describe('test-runner-playwright webkit', function testRunnerPlaywright() {
   });
 });
 
-describe('test-runner-playwright firefox', function testRunnerPlaywright() {
-  this.timeout(100000);
+// we don't run all tests in the windows CI
+if (os.platform() !== 'win32') {
+  describe('test-runner-playwright firefox', function testRunnerPlaywright() {
+    this.timeout(100000);
 
-  function createConfig() {
-    return { browsers: [playwrightLauncher({ product: 'firefox' })] };
-  }
+    function createConfig() {
+      return { browsers: [playwrightLauncher({ product: 'firefox' })] };
+    }
 
-  runIntegrationTests(createConfig, {
-    basic: true,
-    many: true,
-    focus: true,
-    groups: true,
-    // firefox doesn't like parallel in the CI
-    parallel: false,
-    testFailure: true,
-    locationChanged: true,
+    runIntegrationTests(createConfig, {
+      basic: true,
+      many: true,
+      focus: true,
+      groups: true,
+      // firefox doesn't like parallel in the CI
+      parallel: false,
+      testFailure: true,
+      locationChanged: true,
+    });
   });
-});
 
-describe('test-runner-playwright all', function testRunnerPlaywright() {
-  this.timeout(100000);
+  describe('test-runner-playwright all', function testRunnerPlaywright() {
+    this.timeout(100000);
 
-  function createConfig() {
-    return {
-      browsers: [
-        playwrightLauncher({ product: 'chromium' }),
-        playwrightLauncher({ product: 'firefox' }),
-        playwrightLauncher({ product: 'webkit' }),
-      ],
-    };
-  }
+    function createConfig() {
+      return {
+        browsers: [
+          playwrightLauncher({ product: 'chromium' }),
+          playwrightLauncher({ product: 'firefox' }),
+          playwrightLauncher({ product: 'webkit' }),
+        ],
+      };
+    }
 
-  runIntegrationTests(createConfig, {
-    basic: true,
-    many: true,
-    focus: true,
-    groups: true,
-    parallel: false,
-    testFailure: false,
-    locationChanged: false,
+    runIntegrationTests(createConfig, {
+      basic: true,
+      many: true,
+      focus: true,
+      groups: true,
+      parallel: false,
+      testFailure: false,
+      locationChanged: false,
+    });
   });
-});
+}
