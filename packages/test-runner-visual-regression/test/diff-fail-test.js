cc8d011bcab8ab538d6780e4b62eea9b9a550284
Serhii Kulykov <iamkulykov@gmail.com>
2021-11-05 19:15:58 +0200

    test: unskip visual regression plugin tests (#1760)
    * test: update baseline screenshot for Chrome
    
    * test: update visual tests to use background

--- packages/test-runner-visual-regression/test/diff-fail-test.js
+++ packages/test-runner-visual-regression/test/diff-fail-test.js
@@ -1,9 +1,13 @@
 import { visualDiff } from '../browser/commands.mjs';
 
 it('can diff an image', async () => {
-  const element = document.createElement('p');
-  element.textContent = 'Hello world';
-  element.style.color = 'red';
+  const element = document.createElement('div');
+  element.style.padding = '10px';
+  const inner = document.createElement('div');
+  inner.style.backgroundColor = 'red';
+  element.appendChild(inner);
+  inner.style.width = '40px';
+  inner.style.height = '40px';
   document.body.appendChild(element);
 
   await visualDiff(element, 'my-failed-element');
