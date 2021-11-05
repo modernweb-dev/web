78080ad5418bd26d27e58e61c84cd67a234b3d14
web-padawan <iamkulykov@gmail.com>
2021-11-05 17:05:57 +0200

    test: enable Playwright launcher tests on Windows

--- packages/test-runner-playwright/test/playwrightLauncher.test.ts
+++ packages/test-runner-playwright/test/playwrightLauncher.test.ts
@@ -1,4 +1,3 @@
-import os from 'os';
 import { runIntegrationTests } from '../../../integration/test-runner';
 import { playwrightLauncher } from '../src/index';
 
@@ -20,66 +19,63 @@ describe('test-runner-playwright chromium', function testRunnerPlaywright() {
   });
 });
 
-// we don't run all tests in the windows CI
-if (os.platform() !== 'win32') {
-  describe('test-runner-playwright webkit', function testRunnerPlaywright() {
-    this.timeout(100000);
+describe('test-runner-playwright webkit', function testRunnerPlaywright() {
+  this.timeout(100000);
 
-    function createConfig() {
-      return { browsers: [playwrightLauncher({ product: 'webkit' })] };
-    }
+  function createConfig() {
+    return { browsers: [playwrightLauncher({ product: 'webkit' })] };
+  }
 
-    runIntegrationTests(createConfig, {
-      basic: true,
-      many: true,
-      focus: true,
-      groups: true,
-      parallel: true,
-      testFailure: true,
-      locationChanged: true,
-    });
+  runIntegrationTests(createConfig, {
+    basic: true,
+    many: true,
+    focus: true,
+    groups: true,
+    parallel: true,
+    testFailure: true,
+    locationChanged: true,
   });
+});
 
-  describe('test-runner-playwright firefox', function testRunnerPlaywright() {
-    this.timeout(100000);
+describe('test-runner-playwright firefox', function testRunnerPlaywright() {
+  this.timeout(100000);
 
-    function createConfig() {
-      return { browsers: [playwrightLauncher({ product: 'firefox' })] };
-    }
+  function createConfig() {
+    return { browsers: [playwrightLauncher({ product: 'firefox' })] };
+  }
 
-    runIntegrationTests(createConfig, {
-      basic: true,
-      many: true,
-      focus: true,
-      groups: true,
-      // firefox doesn't like parallel in the CI
-      parallel: false,
-      testFailure: true,
-      locationChanged: true,
-    });
+  runIntegrationTests(createConfig, {
+    basic: true,
+    many: true,
+    focus: true,
+    groups: true,
+    // firefox doesn't like parallel in the CI
+    parallel: false,
+    testFailure: true,
+    locationChanged: true,
   });
+});
 
-  describe('test-runner-playwright all', function testRunnerPlaywright() {
-    this.timeout(100000);
+describe('test-runner-playwright all', function testRunnerPlaywright() {
+  this.timeout(100000);
 
-    function createConfig() {
-      return {
-        browsers: [
-          playwrightLauncher({ product: 'chromium' }),
-          playwrightLauncher({ product: 'firefox' }),
-          ...(os.platform() !== 'win32' ? [playwrightLauncher({ product: 'webkit' })] : []),
-        ],
-      };
-    }
+  function createConfig() {
+    return {
+      browsers: [
+        playwrightLauncher({ product: 'chromium' }),
+        playwrightLauncher({ product: 'firefox' }),
+        playwrightLauncher({ product: 'webkit' }),
+      ],
+    };
+  }
 
-    runIntegrationTests(createConfig, {
-      basic: true,
-      many: true,
-      focus: true,
-      groups: true,
-      parallel: false,
-      testFailure: false,
-      locationChanged: false,
-    });
+  runIntegrationTests(createConfig, {
+    basic: true,
+    many: true,
+    focus: true,
+    groups: true,
+    parallel: false,
+    testFailure: false,
+    locationChanged: false,
   });
-}
+});
