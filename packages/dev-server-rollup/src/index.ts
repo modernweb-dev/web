cbbd5fc860a7a520f70c543d1228f74189d17e8b
Bram Gotink <bram@gotink.me>
2021-11-08 11:44:34 +0100

    fix: move @rollup/plugin-node-resolve dependency to fix warning (#1769)

--- packages/dev-server-rollup/src/index.ts
+++ packages/dev-server-rollup/src/index.ts
@@ -1,3 +1,4 @@
+export { RollupNodeResolveOptions, nodeResolve } from '@rollup/plugin-node-resolve';
 export { fromRollup } from './fromRollup';
 export { rollupAdapter } from './rollupAdapter';
 export { rollupBundlePlugin } from './rollupBundlePlugin';
