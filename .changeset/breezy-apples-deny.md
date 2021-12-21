54c6aa82f0b2bfe4b56da912a508494a98ecb63e
Manuel Martin <manuel.martin@gmail.com>
2021-12-21 20:09:16 +0100

    feat: allows the use of exclusion patterns in files globs

---
+++ .changeset/breezy-apples-deny.md
@@ -0,0 +1,5 @@
+---
+'@web/test-runner-core': patch
+---
+
+Allows the use of exclusion patterns in files globs
--- packages/test-runner-core/src/runner/collectTestFiles.ts
+++ packages/test-runner-core/src/runner/collectTestFiles.ts
@@ -2,15 +2,8 @@ import globby from 'globby';
 import path from 'path';
 
 export function collectTestFiles(patterns: string | string[], baseDir = process.cwd()) {
-  const testFiles = new Set<string>();
+  // @ts-ignore
+  const filePatterns = [].concat(patterns).map(pattern => pattern.split(path.sep).join('/'));
 
-  for (const pattern of Array.isArray(patterns) ? patterns : [patterns]) {
-    const normalizedPattern = pattern.split(path.sep).join('/');
-    const foundFiles = globby.sync(normalizedPattern, { cwd: baseDir, absolute: true });
-    for (const file of foundFiles) {
-      testFiles.add(file);
-    }
-  }
-
-  return Array.from(testFiles);
+  return globby.sync(filePatterns, { cwd: baseDir, absolute: true });
 }
