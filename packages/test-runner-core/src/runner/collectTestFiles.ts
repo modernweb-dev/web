f4ed81564c871baba8a01cbd158694c094ef3185
Benny Powers <web@bennypowers.com>
2021-12-23 13:01:06 +0200

    fix(test-runner-core): evaluate files globs together
    Co-Authored-By: Thomas Allmer <d4kmor@gmail.com>
    Co-Authored-By: Manuel Martin <manuel.martin@gmail.com>

--- packages/test-runner-core/src/runner/collectTestFiles.ts
+++ packages/test-runner-core/src/runner/collectTestFiles.ts
@@ -1,16 +1,7 @@
 import globby from 'globby';
-import path from 'path';
+import { sep } from 'path';
 
 export function collectTestFiles(patterns: string | string[], baseDir = process.cwd()) {
-  const testFiles = new Set<string>();
-
-  for (const pattern of Array.isArray(patterns) ? patterns : [patterns]) {
-    const normalizedPattern = pattern.split(path.sep).join('/');
-    const foundFiles = globby.sync(normalizedPattern, { cwd: baseDir, absolute: true });
-    for (const file of foundFiles) {
-      testFiles.add(file);
-    }
-  }
-
-  return Array.from(testFiles);
+  const normalizedPatterns = [patterns].flat().map(p => p.split(sep).join('/'));
+  return globby.sync(normalizedPatterns, { cwd: baseDir, absolute: true });
 }
