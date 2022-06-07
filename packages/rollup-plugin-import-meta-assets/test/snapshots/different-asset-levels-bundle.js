6ab8a0ad4ff5bbac28de088519cf42c2b8eae7d2
Carlos Precioso <github@precioso.design>
2022-06-07 14:48:32 +0200

    test(rollup-plugin-import-meta-assets): update tests

--- packages/rollup-plugin-import-meta-assets/test/snapshots/different-asset-levels-bundle.js
+++ packages/rollup-plugin-import-meta-assets/test/snapshots/different-asset-levels-bundle.js
@@ -1,14 +1,14 @@
 const nameOne = 'one-name';
-const imageOne = new URL(new URL('assets/one-deep-d40c1b4b.svg', import.meta.url).href, import.meta.url).href;
+const imageOne = new URL(new URL('assets/one-deep-d40c1b4b.svg', import.meta.url).href).href;
 
 const nameTwo = 'two-name';
-const imageTwo = new URL(new URL('assets/two-deep-e73b0d96.svg', import.meta.url).href, import.meta.url).href;
+const imageTwo = new URL(new URL('assets/two-deep-e73b0d96.svg', import.meta.url).href).href;
 
 const nameThree = 'three-name';
-const imageThree = new URL(new URL('assets/three-deep-801763e8.svg', import.meta.url).href, import.meta.url).href;
+const imageThree = new URL(new URL('assets/three-deep-801763e8.svg', import.meta.url).href).href;
 
 const nameFour = 'four-name';
-const imageFour = new URL(new URL('assets/four-deep-c65478aa.svg', import.meta.url).href, import.meta.url).href;
+const imageFour = new URL(new URL('assets/four-deep-c65478aa.svg', import.meta.url).href).href;
 
 console.log({
   [nameOne]: imageOne,
