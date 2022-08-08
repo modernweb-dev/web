af9c9b35b6877533630b15accb6eb9f716871bc9
thepassle <pascalschilp@gmail.com>
2022-08-08 13:57:00 +0200

    chore: fixes

--- packages/dev-server-storybook/src/shared/stories/injectExportsOrder.ts
+++ packages/dev-server-storybook/src/shared/stories/injectExportsOrder.ts
@@ -2,7 +2,7 @@ import { parse } from 'es-module-lexer';
 
 export async function injectExportsOrder(source: string, filePath: string) {
   const [, exports] = await parse(source, filePath);
-  if (exports.includes('__namedExportsOrder')) {
+  if (exports.some(e => e.n === '__namedExportsOrder')) {
     // user has defined named exports already
     return null;
   }
