7f8265560b225e12034a4cd1c09e772085ebc60c
Westbrook Johnson <westbrook.johnson@gmail.com>
2022-08-12 13:11:18 -0400

    Keep "Default" stories
    Using capital "D" `Default` is a super common pattern in Storybook creation. While we're pushing for `Regular` or similar in some of our CLI tooling, not allowing it at all is a surprising side effect for consumers of this package.

--- packages/dev-server-storybook/src/shared/stories/injectExportsOrder.ts
+++ packages/dev-server-storybook/src/shared/stories/injectExportsOrder.ts
@@ -7,7 +7,7 @@ export async function injectExportsOrder(source: string, filePath: string) {
     return null;
   }
 
-  const orderedExports = exports.filter(e => e?.n?.toLowerCase() !== 'default');
+  const orderedExports = exports.filter(e => e?.n !== 'default');
   const exportsArray = `['${orderedExports.map(({ n }) => n).join("', '")}']`;
 
   return `${source};\nexport const __namedExportsOrder = ${exportsArray};`;
