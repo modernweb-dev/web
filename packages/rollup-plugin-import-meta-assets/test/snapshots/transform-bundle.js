e2c8e01699f5a50d9ebde78cf4af1d3aedd27adc
Westbrook Johnson <westbrook.johnson@gmail.com>
2023-03-14 22:18:28 -0400

    test: take snapshot updates in rollup-plugin-import-meta

--- packages/rollup-plugin-import-meta-assets/test/snapshots/transform-bundle.js
+++ packages/rollup-plugin-import-meta-assets/test/snapshots/transform-bundle.js
@@ -1,8 +1,8 @@
-const justUrlObject = new URL(new URL('assets/one-d81655b9.svg', import.meta.url).href, import.meta.url);
-const href = new URL(new URL('assets/two-00516e7a.svg', import.meta.url).href, import.meta.url).href;
-const pathname = new URL(new URL('assets/three-0ba6692d.svg', import.meta.url).href, import.meta.url).pathname;
-const searchParams = new URL(new URL('assets/four-a00e2e1d.svg', import.meta.url).href, import.meta.url).searchParams;
-const someJpg = new URL(new URL('assets/image-d6eb190c.jpg', import.meta.url).href, import.meta.url);
+const justUrlObject = new URL(new URL('assets/one-2d604b7f.svg', import.meta.url).href, import.meta.url);
+const href = new URL(new URL('assets/two-4f4a5196.svg', import.meta.url).href, import.meta.url).href;
+const pathname = new URL(new URL('assets/three-cb83a3f2.svg', import.meta.url).href, import.meta.url).pathname;
+const searchParams = new URL(new URL('assets/four-97848687.svg', import.meta.url).href, import.meta.url).searchParams;
+const someJpg = new URL(new URL('assets/image-c160fa1b.jpg', import.meta.url).href, import.meta.url);
 
 console.log({
   justUrlObject,
