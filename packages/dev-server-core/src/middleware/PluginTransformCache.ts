83728f6b6730eb01ffe98d35d38f44d9141ba456
43081j <43081j@users.noreply.github.com>
2023-03-20 18:51:39 +0000

    feat (dev-server-core): upgrade lru-cache

--- packages/dev-server-core/src/middleware/PluginTransformCache.ts
+++ packages/dev-server-core/src/middleware/PluginTransformCache.ts
@@ -30,11 +30,11 @@ export class PluginTransformCache {
   private lruCache: LRUCache<string, CacheEntry>;
 
   constructor(private fileWatcher: FSWatcher, private rootDir: string) {
-    this.lruCache = new LRUCache({
-      length: (e, key) => e.body.length + (key ? key.length : 0),
+    this.lruCache = new LRUCache<string, CacheEntry>({
+      sizeCalculation: (e, key) => e.body.length + (key ? key.length : 0),
       max: 52428800,
       noDisposeOnSet: true,
-      dispose: cacheKey => {
+      dispose: (_value, cacheKey) => {
         // remove file path -> url mapping when we are no longer caching it
         for (const [filePath, cacheKeysForFilePath] of this.cacheKeysPerFilePath.entries()) {
           if (cacheKeysForFilePath.has(cacheKey)) {
@@ -50,7 +50,7 @@ export class PluginTransformCache {
       const cacheKeys = this.cacheKeysPerFilePath.get(filePath);
       if (cacheKeys) {
         for (const cacheKey of cacheKeys) {
-          this.lruCache.del(cacheKey);
+          this.lruCache.delete(cacheKey);
         }
       }
     };
