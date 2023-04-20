3177d3ebe85520f518f185fcca3b9fbcf28fa024
Kristján Oddsson <koddsson@gmail.com>
2023-04-20 19:06:32 +0200

    Unskip test not that we have upgraded the rollup plugin

--- packages/dev-server-rollup/test/node/plugins/node-resolve.test.ts
+++ packages/dev-server-rollup/test/node/plugins/node-resolve.test.ts
@@ -146,8 +146,7 @@ describe('@rollup/plugin-node-resolve', () => {
     }
   });
 
-  // works with @rollup/plugin-commonjs 22.x but that breaks other tests
-  it.skip('node modules resolved outside root directory are rewritten with commonjs', async () => {
+  it('node modules resolved outside root directory are rewritten with commonjs', async () => {
     const { server, host } = await createTestServer({
       rootDir: path.resolve(__dirname, '..', 'fixtures', 'resolve-outside-dir', 'src'),
       plugins: [commonjs(), nodeResolve()],
