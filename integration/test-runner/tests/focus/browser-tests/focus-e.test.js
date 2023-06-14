11185b23ae680d04c28ddabc3ac7aaa35776117e
Kristján Oddsson <koddsson@gmail.com>
2023-06-14 11:48:49 +0200

    Remove debug from focus test

--- integration/test-runner/tests/focus/browser-tests/focus-e.test.js
+++ integration/test-runner/tests/focus/browser-tests/focus-e.test.js
@@ -4,30 +4,14 @@ it('can run a test with focus e', async () => {
   const input = document.createElement('input');
   document.body.appendChild(input);
 
-  console.log('firing event!')
-
   let firedEvent = false;
   input.addEventListener('focus', () => {
-    console.log('event fired');
     firedEvent = true;
   });
   input.focus();
-  
-  console.log('focused!')
 
   // await 2 frames for IE11
   await new Promise(requestAnimationFrame);
   await new Promise(requestAnimationFrame);
-  
-  console.log('awaited for animation frames!')
-
-  await new Promise(resolve => {
-    setTimeout(resolve, 300);
-  })
-
-  console.log(document.body, input);
-
-  console.log(firedEvent);
-
   expect(firedEvent).to.be.true;
 });
