3fae385bd8f46b597913bc4ab9be0b31a0255b40
Thomas Allmer <d4kmor@gmail.com>
2023-07-02 14:08:42 +0200

    chore: move to npm workspaces

--- packages/browser-logs/test/serialize-deserialize.test.ts
+++ packages/browser-logs/test/serialize-deserialize.test.ts
@@ -10,7 +10,7 @@ const { sep } = path;
 const serializeScript = fs.readFileSync(require.resolve('../dist/serialize.js'), 'utf-8');
 const defaultOptions = { browserRootDir: __dirname, cwd: __dirname };
 
-describe('serialize deserialize', () => {
+describe.only('serialize deserialize', () => {
   let browser: Browser;
   let page: Page;
   before(async () => {
