b86488fde1655421332b24da951d84f81f1c2bff
Jason Mobley <jason.mobley@genesys.com>
2023-10-30 09:46:22 -0400

    chore: revert test change

--- packages/test-runner-core/test/src/runner/TestRunner.test.ts
+++ packages/test-runner-core/test/src/runner/TestRunner.test.ts
@@ -251,7 +251,10 @@ describe('TestRunner', function () {
     it('can ignore files via string[] globs', async () => {
       const normalize = (x: string): string => x.replace(/\//g, path.sep);
       const { runner } = await createTestRunner({
-        files: ['./test/fixtures/**/*.test.js', '!./test/fixtures/group-c/*'].map(normalize),
+        files: [
+          'packages/test-runner-core/test/fixtures/**/*.test.js',
+          '!packages/test-runner-core/test/fixtures/group-c/*',
+        ].map(normalize),
       });
 
       const sessions = Array.from(runner.sessions.all());
