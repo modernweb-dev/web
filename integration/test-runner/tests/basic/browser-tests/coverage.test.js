8484bd3f7ccc3b4b326c3f327484a7ad5e57a230
Andy Weiss <andy.weiss@reddit.com>
2024-08-15 09:57:41 -0400

    Fix tests

--- integration/test-runner/tests/basic/browser-tests/coverage.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from '../../../../../node_modules/@esm-bundle/chai/esm/chai.js';
-
-describe('basic test', () => {
-  it('works', () => {
-    window.__coverage__ = 'something';
-    expect(true).to.equal(true);
-  });
-});
