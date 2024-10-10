03cbb484b3b1b6c42437cedd896df6642aab4a9d
Luca Haneklau <git@luca.lsys.ac>
2024-10-10 14:49:38 +0200

    fix: test result serialization with immutable or unserializable errors

---
+++ integration/test-runner/tests/test-failure/browser-tests/fail-unserializable-actual.test.js
@@ -0,0 +1,21 @@
+import { expect } from '../../../../../node_modules/@esm-bundle/chai/esm/chai.js';
+
+xit('readonly actual', function() {
+  const fixture = Object.freeze({x: {}});
+  expect(fixture).to.equal(null);
+})
+
+it('symbol actual', function() {
+  const fixture = Symbol('foo');
+  expect(fixture).to.equal(null);
+})
+
+it('globalThis actual', function() {
+  const fixture = globalThis;
+  expect(fixture).to.equal(null);
+})
+
+it('HTMLElement actual', function() {
+  const fixture = document.createElement('div');
+  expect(fixture).to.equal(null);
+})
