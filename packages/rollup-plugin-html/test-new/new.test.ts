28b43402e3783a76f236d20b95407cbcb70dbd38
Mikhail Bashkirov <bashmish@gmail.com>
2025-12-22 18:04:12 +0400

    WIP6

--- packages/rollup-plugin-html/test-new/new.test.ts
+++ packages/rollup-plugin-html/test-new/new.test.ts
@@ -3073,4 +3073,217 @@ describe('rollup-plugin-html', () => {
       }
     `);
   });
+
+  it('rewrites paths according to assetFileNames', async () => {
+    const rootDir = createApp({
+      'node_modules/ing-web/fonts/font.woff2': 'font.woff',
+      'node_modules/ing-web/global.css': css`
+        @font-face {
+          font-family: Font;
+          src: url('fonts/font.woff2') format('woff2');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+      `,
+      'assets/images/image.png': 'image.png',
+      'assets/styles.css': css`
+        #a {
+          background-image: url('images/image.png');
+        }
+      `,
+      'src/main.js': js`
+        const imageUrl = new URL('../assets/images/image.png', import.meta.url).href;
+      `,
+      'index.html': html`
+        <html>
+          <head>
+            <link rel="stylesheet" href="./node_modules/ing-web/global.css" />
+            <link rel="stylesheet" href="./assets/styles.css" />
+            <link
+              rel="preload"
+              href="./node_modules/ing-web/fonts/font.woff2"
+              as="font"
+              type="font/woff2"
+            />
+          </head>
+          <body>
+            <img src="./assets/images/image.png" />
+          </body>
+        </html>
+      `,
+    });
+
+    const config = {
+      plugins: [
+        rollupPluginHTML({
+          rootDir,
+          input: './index.html',
+        }),
+      ],
+    };
+
+    const build = await rollup(config);
+    const { chunks, assets } = await generateTestBundle(build, {
+      ...outputConfig,
+      assetFileNames: 'static/[name].immutable.[hash][extname]',
+    });
+
+    expect(Object.keys(chunks)).to.have.lengthOf(1);
+    expect(Object.keys(assets)).to.have.lengthOf(5);
+
+    expect(assets).to.have.keys([
+      'static/font.immutable.C5MNjX-h.woff2',
+      'static/global.immutable.DB0fKkjs.css',
+      'static/image.immutable.7xJLr_7N.png',
+      'static/styles.immutable.D4tZXVv0.css',
+      'index.html',
+    ]);
+
+    expect(assets['index.html']).to.equal(html`
+      <html>
+        <head>
+          <link rel="stylesheet" href="static/global.immutable.DB0fKkjs.css" />
+          <link rel="stylesheet" href="static/styles.immutable.D4tZXVv0.css" />
+          <link
+            rel="preload"
+            href="static/font.immutable.C5MNjX-h.woff2"
+            as="font"
+            type="font/woff2"
+          />
+        </head>
+        <body>
+          <img src="static/image.immutable.7xJLr_7N.png" />
+        </body>
+      </html>
+    `);
+
+    expect(assets['static/global.immutable.DB0fKkjs.css']).to.equal(css`
+      @font-face {
+        font-family: Font;
+        src: url('font.immutable.C5MNjX-h.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+      }
+    `);
+
+    expect(assets['static/styles.immutable.D4tZXVv0.css']).to.equal(css`
+      #a {
+        background-image: url('image.immutable.7xJLr_7N.png');
+      }
+    `);
+  });
+
+  it('resolves paths by using publicPath when assetFileNames puts assets in different dirs', async () => {
+    const rootDir = createApp({
+      'node_modules/ing-web/fonts/font.woff2': 'font.woff',
+      'node_modules/ing-web/global.css': css`
+        @font-face {
+          font-family: Font;
+          src: url('fonts/font.woff2') format('woff2');
+          font-weight: normal;
+          font-style: normal;
+          font-display: swap;
+        }
+      `,
+      'assets/images/image.png': 'image.png',
+      'assets/styles.css': css`
+        #a {
+          background-image: url('images/image.png');
+        }
+      `,
+      'src/main.js': js`
+        const imageUrl = new URL('../assets/images/image.png', import.meta.url).href;
+      `,
+      'index.html': html`
+        <html>
+          <head>
+            <link rel="stylesheet" href="./node_modules/ing-web/global.css" />
+            <link rel="stylesheet" href="./assets/styles.css" />
+            <link
+              rel="preload"
+              href="./node_modules/ing-web/fonts/font.woff2"
+              as="font"
+              type="font/woff2"
+            />
+          </head>
+          <body>
+            <img src="./assets/images/image.png" />
+          </body>
+        </html>
+      `,
+    });
+
+    const config = {
+      plugins: [
+        rollupPluginHTML({
+          rootDir,
+          input: './index.html',
+          publicPath: '/static/',
+        }),
+      ],
+    };
+
+    const build = await rollup(config);
+    const { chunks, assets } = await generateTestBundle(build, {
+      ...outputConfig,
+      assetFileNames: assetInfo => {
+        const name = assetInfo.names[0] || '';
+        if (name.endsWith('.woff2')) {
+          return 'fonts/[name].immutable.[hash][extname]';
+        } else if (name.endsWith('.css')) {
+          return 'styles/[name].immutable.[hash][extname]';
+        } else if (name.endsWith('.png')) {
+          return 'images/[name].immutable.[hash][extname]';
+        }
+        return '[name].immutable.[hash][extname]';
+      },
+    });
+
+    expect(Object.keys(chunks)).to.have.lengthOf(1);
+    expect(Object.keys(assets)).to.have.lengthOf(5);
+
+    expect(assets).to.have.keys([
+      'fonts/font.immutable.C5MNjX-h.woff2',
+      'styles/global.immutable.B3Q0ucg4.css',
+      'images/image.immutable.7xJLr_7N.png',
+      'styles/styles.immutable.C3Z0Fs2-.css',
+      'index.html',
+    ]);
+
+    expect(assets['index.html']).to.equal(html`
+      <html>
+        <head>
+          <link rel="stylesheet" href="/static/styles/global.immutable.B3Q0ucg4.css" />
+          <link rel="stylesheet" href="/static/styles/styles.immutable.C3Z0Fs2-.css" />
+          <link
+            rel="preload"
+            href="/static/fonts/font.immutable.C5MNjX-h.woff2"
+            as="font"
+            type="font/woff2"
+          />
+        </head>
+        <body>
+          <img src="/static/images/image.immutable.7xJLr_7N.png" />
+        </body>
+      </html>
+    `);
+
+    expect(assets['styles/global.immutable.B3Q0ucg4.css']).to.equal(css`
+      @font-face {
+        font-family: Font;
+        src: url('/static/fonts/font.immutable.C5MNjX-h.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+      }
+    `);
+
+    expect(assets['styles/styles.immutable.C3Z0Fs2-.css']).to.equal(css`
+      #a {
+        background-image: url('/static/images/image.immutable.7xJLr_7N.png');
+      }
+    `);
+  });
 });
