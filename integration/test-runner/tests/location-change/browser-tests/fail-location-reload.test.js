550a07d7db9c18305c4e22dc5cea629953e6c845
Mikhail Bashkirov <bashmish@gmail.com>
2026-07-16 15:59:24 +0200

    test: improve reload test stability

--- integration/test-runner/tests/location-change/browser-tests/fail-location-reload.test.js
+++ integration/test-runner/tests/location-change/browser-tests/fail-location-reload.test.js
@@ -1,6 +1,11 @@
-setTimeout(() => {
-  window.location.reload();
-}, 100);
+if (!sessionStorage.getItem('reloaded')) {
+  sessionStorage.setItem('reloaded', 'true');
+  setTimeout(() => {
+    window.location.reload();
+  }, 100);
+} else {
+  sessionStorage.removeItem('reloaded');
+}
 
 it('x', async function test() {
   this.timeout(20000);
