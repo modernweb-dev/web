0f5cfec54c01bcb3e78cc575ff96a21843300bda
Mikhail Bashkirov <bashmish@gmail.com>
2026-07-22 12:46:58 +0200

    test: reduce each test waiting to fit into overall suite timeout

--- integration/test-runner/tests/location-change/browser-tests/fail-location-reload.test.js
+++ integration/test-runner/tests/location-change/browser-tests/fail-location-reload.test.js
@@ -8,6 +8,6 @@ if (!sessionStorage.getItem('reloaded')) {
 }
 
 it('x', async function test() {
-  this.timeout(20000);
-  await new Promise(r => setTimeout(r, 10000));
+  this.timeout(10000);
+  await new Promise(r => setTimeout(r, 5000));
 });
